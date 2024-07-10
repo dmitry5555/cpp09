688b227d045a6a76b420afa0222d43da74ede4f4
Dmitrii Larionov <mikrolux@gmail.com>
2024-07-10 17:09:33 +0700

    first commit

---
+++ 00/src/main.cpp
@@ -0,0 +1,15 @@
+#include "easyfind.hpp"
+
+
+int main(int argc, char **argv) {
+	
+    std::vector<int> vec;
+   
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+    vec.push_back(4);
+
+	easyfind(vec, 4);
+    return 0;
+}
